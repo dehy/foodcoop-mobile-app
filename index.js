73e242cd66636f6d7a205b8e2bd14a678d1c41df
Arnaud de Mouhy <arnaud.demouhy@akerbis.com>
2019-02-11 21:55:15 +0100

    Activer Sentry si app pas endev

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ import momentFr from 'moment/locale/fr';
 import './src/utils/utils';
 
 moment.locale('fr');
-// Sentry.config('https://4924f3cfe9d84a4ba43a95ed1a959f1d:ddf962666b804d018cd8fe5b68b332f4@sentry.akerbis.com/9').install();
+if (!__DEV__) {
+    Sentry.config('https://4924f3cfe9d84a4ba43a95ed1a959f1d:ddf962666b804d018cd8fe5b68b332f4@sentry.akerbis.com/9').install();
+}
 
 registerScreens();
 
