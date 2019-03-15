61516b0684b78200afb1fee2dc8f27cffb70b45d
Arnaud de Mouhy <arnaud.demouhy@supercoop.fr>
2019-03-15 10:59:06 +0100

    Reconstruction du projet android

--- android/app/src/main/java/fr/supercoop/MainApplication.java
+++ android/app/src/main/java/fr/supercoop/MainApplication.java
@@ -1,41 +1,29 @@
 package fr.supercoop;
 
-import android.content.Context;
-import android.support.multidex.MultiDex;
-
 import com.actionsheet.ActionSheetPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.horcrux.svg.SvgPackage;
 import com.oblador.keychain.KeychainPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.psykar.cookiemanager.CookieManagerPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
-
-import co.apptailor.googlesignin.RNGoogleSigninPackage;
-import io.sentry.RNSentryPackage;
-
-import org.pgsqlite.SQLitePluginPackage;
-import org.reactnative.camera.RNCameraPackage;
-
-import com.facebook.react.ReactNativeHost;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
 import com.reactnativenavigation.react.ReactGateway;
 import com.zmxv.RNSound.RNSoundPackage;
 
+import org.pgsqlite.SQLitePluginPackage;
+import org.reactnative.camera.RNCameraPackage;
+
 import java.util.Arrays;
 import java.util.List;
 
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
+import io.sentry.RNSentryPackage;
+
 public class MainApplication extends NavigationApplication {
-
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        MultiDex.install(this);
-    }
-
     @Override
     protected ReactGateway createReactGateway() {
         ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
@@ -49,59 +37,28 @@ public class MainApplication extends NavigationApplication {
 
     @Override
     public boolean isDebug() {
-        return BuildConfig.DEBUG;
+      return BuildConfig.DEBUG;
     }
 
     protected List<ReactPackage> getPackages() {
-        // Add additional packages you require here
-        // No need to add RnnPackage and MainReactPackage
-        return Arrays.<ReactPackage>asList(
-                new RNGoogleSigninPackage(),
-                new RNCWebViewPackage(),
-                new RNCameraPackage(),
-                new RNSentryPackage(),
-                new SvgPackage(),
-                new KeychainPackage(),
-                new RNSoundPackage(),
-                new VectorIconsPackage(),
-                new SQLitePluginPackage(),
-                new ActionSheetPackage(),
-                new CookieManagerPackage()
-        );
+      return Arrays.<ReactPackage>asList(
+        new MainReactPackage(),
+        new RNGoogleSigninPackage(),
+        new RNCWebViewPackage(),
+        new RNCameraPackage(),
+        new RNSentryPackage(),
+        new SvgPackage(),
+        new KeychainPackage(),
+        new RNSoundPackage(),
+        new VectorIconsPackage(),
+        new SQLitePluginPackage(),
+        new ActionSheetPackage(),
+        new CookieManagerPackage()
+      );
     }
 
     @Override
     public List<ReactPackage> createAdditionalReactPackages() {
-        return getPackages();
-    }
-
-    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-            return BuildConfig.DEBUG;
-        }
-
-        @Override
-        protected List<ReactPackage> getPackages() {
-            return Arrays.<ReactPackage>asList(
-                    new MainReactPackage()
-            );
-        }
-
-        @Override
-        protected String getJSMainModuleName() {
-            return "index";
-        }
-    };
-
-    @Override
-    public ReactNativeHost getReactNativeHost() {
-        return mReactNativeHost;
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        SoLoader.init(this, /* native exopackage */ false);
+      return getPackages();
     }
 }
