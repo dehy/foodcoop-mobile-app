59f0419e2b511394dde35c2babbeb903e4e2034b
Arnaud de Mouhy <arnaud.demouhy@supercoop.fr>
2019-11-18 16:37:39 +0100

    Updated to React Native v0.60.6

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ import 'reflect-metadata';
 import { Navigation } from "react-native-navigation";
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { registerScreens } from './src/utils/screens';
-import { Sentry } from 'react-native-sentry';
+import * as Sentry from '@sentry/react-native';
 import moment from 'moment';
 import momentFr from 'moment/locale/fr';
 import Database from './src/utils/Database';
 
 moment.locale('fr');
 if (!__DEV__) {
-    Sentry.config('https://4924f3cfe9d84a4ba43a95ed1a959f1d:ddf962666b804d018cd8fe5b68b332f4@sentry.akerbis.com/9').install();
+    Sentry.init({ 
+        dsn: 'https://4924f3cfe9d84a4ba43a95ed1a959f1d:ddf962666b804d018cd8fe5b68b332f4@sentry.akerbis.com/9', 
+      });
 }
 
 registerScreens();
