aca2993a3ec5c814c6c2c9f18f5cbfc11969567b
Arnaud de Mouhy <arnaud.demouhy@supercoop.fr>
2019-12-16 22:43:53 +0100

    Linting

--- src/factories/Odoo/PurchaseOrderFactory.ts
+++ src/factories/Odoo/PurchaseOrderFactory.ts
@@ -1,27 +1,19 @@
-"use strict";
+'use strict';
 
-import moment from "moment";
-import Odoo from "../../utils/Odoo";
-import PurchaseOrder from "../../entities/Odoo/PurchaseOrder";
+import moment from 'moment';
+import Odoo from '../../utils/Odoo';
+import PurchaseOrder from '../../entities/Odoo/PurchaseOrder';
 
 export default class PurchaseOrderFactory {
-  public static PurchaseOrderFromResponse(
-    response: OdooApiPurchaseOrder
-  ): PurchaseOrder {
-    const purchaseOrder = new PurchaseOrder();
-    purchaseOrder.id = response.id;
-    purchaseOrder.name = response.name;
-    purchaseOrder.partnerId = response.partner_id![0];
-    purchaseOrder.partnerName = response.partner_id![1];
-    purchaseOrder.orderDate = moment(
-      response.date_order,
-      Odoo.DATETIME_FORMAT
-    ).toDate();
-    purchaseOrder.plannedDeliveryDate = moment(
-      response.date_planned,
-      Odoo.DATETIME_FORMAT
-    ).toDate();
+    public static PurchaseOrderFromResponse(response: OdooApiPurchaseOrder): PurchaseOrder {
+        const purchaseOrder = new PurchaseOrder();
+        purchaseOrder.id = response.id;
+        purchaseOrder.name = response.name;
+        purchaseOrder.partnerId = response.partner_id && response.partner_id[0];
+        purchaseOrder.partnerName = response.partner_id && response.partner_id[1];
+        purchaseOrder.orderDate = moment(response.date_order, Odoo.DATETIME_FORMAT).toDate();
+        purchaseOrder.plannedDeliveryDate = moment(response.date_planned, Odoo.DATETIME_FORMAT).toDate();
 
-    return purchaseOrder;
-  }
+        return purchaseOrder;
+    }
 }
