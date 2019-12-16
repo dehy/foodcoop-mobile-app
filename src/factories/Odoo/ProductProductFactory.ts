6ef2264cfbcf54492f5bf18f3371c5c3d89539a0
Arnaud de Mouhy <arnaud.demouhy@supercoop.fr>
2019-12-16 22:43:53 +0100

    Linting

--- src/factories/Odoo/ProductProductFactory.ts
+++ src/factories/Odoo/ProductProductFactory.ts
@@ -1,22 +1,20 @@
-"use strict";
+'use strict';
 
-import ProductProduct from "../../entities/Odoo/ProductProduct";
+import ProductProduct from '../../entities/Odoo/ProductProduct';
 
 export default class ProductProductFactory {
-  public static ProductProductFromResponse(
-    response: OdooApiProductProduct
-  ): ProductProduct {
-    const product = new ProductProduct();
-    product.id = response.id;
-    product.barcode = response.barcode;
-    product.name = response.name;
-    product.image = response.image != null ? response.image : undefined;
-    product.qty_available = response.qty_available;
-    product.uom_id = response.uom_id;
-    product.lst_price = response.lst_price;
-    product.weight_net = response.weight_net;
-    product.volume = response.volume;
+    public static ProductProductFromResponse(response: OdooApiProductProduct): ProductProduct {
+        const product = new ProductProduct();
+        product.id = response.id;
+        product.barcode = response.barcode;
+        product.name = response.name;
+        product.image = response.image != null ? response.image : undefined;
+        product.qty_available = response.qty_available;
+        product.uom_id = response.uom_id;
+        product.lst_price = response.lst_price;
+        product.weight_net = response.weight_net;
+        product.volume = response.volume;
 
-    return product;
-  }
+        return product;
+    }
 }
