f4ecfb47c5576884a9b1ab45795a1e322739ee33
Arnaud de Mouhy <arnaud.demouhy@supercoop.fr>
2020-01-15 22:51:32 +0100

    Updated build configuration
    Sentry release + disabled minification

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ moment.locale('fr');
 if (!__DEV__) {
     Sentry.init({
         dsn: 'https://ade630e97f5947188b6bad45497c9462@sentry.admds.net/2',
-        release: `sp_mobile_app-${DeviceInfo.getVersion()}`,
     });
+    Sentry.setRelease(`sp_mobile_app-${DeviceInfo.getVersion()}`);
 }
 
 registerScreens();
