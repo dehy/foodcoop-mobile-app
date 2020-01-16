8d806f797ffdb27431ba46020b2f0367ec1ed190
Arnaud de Mouhy <arnaud.demouhy@supercoop.fr>
2020-01-16 13:35:13 +0100

    Removed Sentry

--- index.js
+++ index.js
@@ -6,17 +6,17 @@
 import { Navigation } from 'react-native-navigation';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { registerScreens } from './src/utils/screens';
-import * as Sentry from '@sentry/react-native';
+// import * as Sentry from '@sentry/react-native';
 import moment from 'moment';
 import Database from './src/utils/Database';
-import DeviceInfo from 'react-native-device-info';
+// import DeviceInfo from 'react-native-device-info';
 
 moment.locale('fr');
 if (!__DEV__) {
-    Sentry.init({
-        dsn: 'https://ade630e97f5947188b6bad45497c9462@sentry.admds.net/2',
-    });
-    Sentry.setRelease(`sp_mobile_app-${DeviceInfo.getVersion()}`);
+    // Sentry.init({
+    //     dsn: 'https://ade630e97f5947188b6bad45497c9462@sentry.admds.net/2',
+    // });
+    // Sentry.setRelease(`sp_mobile_app-${DeviceInfo.getVersion()}`);
 }
 
 registerScreens();
