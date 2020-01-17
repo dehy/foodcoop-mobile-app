803cf3b7f9d4c8c02a524ec2bcfbff9241972525
Arnaud de Mouhy <arnaud.demouhy@supercoop.fr>
2020-01-17 14:27:59 +0100

    Chores

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,8 +1,8 @@
-const { getDefaultConfig } = require("metro-config");
+const { getDefaultConfig } = require('metro-config');
 
 module.exports = (async () => {
     const {
-        resolver: { sourceExts, assetExts }
+        resolver: { sourceExts, assetExts },
     } = await getDefaultConfig();
     return {
         getTransformModulePath() {
@@ -12,11 +12,11 @@ module.exports = (async () => {
             return ['ts', 'tsx'];
         },
         transformer: {
-            babelTransformerPath: require.resolve("react-native-svg-transformer")
+            babelTransformerPath: require.resolve('react-native-svg-transformer'),
         },
         resolver: {
-            assetExts: assetExts.filter(ext => ext !== "svg"),
-            sourceExts: [...sourceExts, "svg"]
-        }
+            assetExts: assetExts.filter(ext => ext !== 'svg'),
+            sourceExts: [...sourceExts, 'svg'],
+        },
     };
 })();
