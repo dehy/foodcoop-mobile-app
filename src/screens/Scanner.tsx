f4eb0510813627066a1a9abbcff41ef8a4be2f5c
Arnaud de Mouhy <arnaud.demouhy@supercoop.fr>
2020-04-27 23:29:06 +0200

    Fixed inventory quantity float on iOS

--- src/screens/Scanner.tsx
+++ src/screens/Scanner.tsx
@@ -458,7 +458,7 @@ Il a été associé à un produit nommé "${odooProductProduct.name}"`;
             if (!this.articleQuantityValue) {
                 throw new Error();
             }
-            quantity = parseFloat(this.articleQuantityValue);
+            quantity = parseFloat(this.articleQuantityValue.replace(',', '.'));
         } catch (e) {
             Alert.alert('Valeur incorrecte', 'Cela ne ressemble pas à un nombre.');
             return;
