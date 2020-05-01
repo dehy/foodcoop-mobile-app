5efc06d182f31bf148ddcda26acdcfe8bd438b29
Arnaud de Mouhy <arnaud.demouhy@supercoop.fr>
2020-05-01 17:16:35 +0200

    Added git post-checkout with yarn install

--- postinstall.sh
+++ postinstall.sh
@@ -4,8 +4,14 @@
 
 set -eu
 
+echo "+ Executing Yarn's postinstall"
+
+echo "++ Executing patch-package"
 patch-package
 
 if [ "$(uname -s)" = "Darwin" ]; then
+	echo "++ Executing pod update"
 	pod update --project-directory=./ios/
+else
+	echo "!! Not on macOS, skipping pod update"
 fi
