739beac7527b4ddb94957bf6d78116c58ff98a6e
Arnaud de Mouhy <arnaud.demouhy@supercoop.fr>
2020-06-21 18:15:37 +0200

    Removed all Sentry reference

--- index.js
+++ index.js
@@ -6,20 +6,12 @@
 import { Navigation } from 'react-native-navigation';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { registerScreens } from './src/utils/screens';
-import * as Sentry from '@sentry/react-native';
 import moment from 'moment';
 import 'moment/locale/fr';
 import Database from './src/utils/Database';
 import { readableVersion, systemName } from './src/utils/helpers';
 
 moment.locale('fr');
-if (!__DEV__) {
-    Sentry.init({
-        dsn: 'https://ade630e97f5947188b6bad45497c9462@sentry.admds.net/2',
-    });
-    Sentry.setRelease(`mobile-app-${readableVersion}`);
-    Sentry.setDist(systemName);
-}
 
 registerScreens();
 Database.connect();
