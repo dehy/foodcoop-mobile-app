90570c63d1912c3b6e007d28453afd9908711ab0
Arnaud de Mouhy <arnaud.demouhy@supercoop.fr>
2020-10-12 17:10:52 +0200

    chore: using install instead of update for cocoapods

--- postinstall.sh
+++ postinstall.sh
@@ -10,8 +10,10 @@ echo "++ Executing patch-package"
 patch-package
 
 if [ "$(uname -s)" = "Darwin" ]; then
-	echo "++ Executing pod update"
-	pod update --project-directory=./ios/
+	echo "++ Executing pod install"
+	set -x
+	pod install --project-directory=./ios/
+	set +x
 else
-	echo "!! Not on macOS, skipping pod update"
+	echo "!! Not on macOS, skipping pod install"
 fi
