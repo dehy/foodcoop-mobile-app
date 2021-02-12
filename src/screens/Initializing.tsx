1c71a25820cbb731a642d4ed6de8e600a3c509b1
Arnaud de Mouhy <arnaud.demouhy@supercoop.fr>
2021-02-12 09:22:14 +0100

    feat: random loading messages

--- src/screens/Initializing.tsx
+++ src/screens/Initializing.tsx
@@ -13,12 +13,33 @@ const styles = StyleSheet.create({
     welcome: {
         fontSize: 28,
     },
+    quoteView: {
+        backgroundColor: '#EEEEEE',
+        borderRadius: 10,
+        padding: 10,
+        marginTop: 20,
+    },
+    quoteText: {
+        color: '#000000',
+    },
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
 });
+
+const welcomeMessages = [
+    'Alignement des astres...',
+    "En train de faire l'impossible...",
+    "Comment vas-tu aujourd'hui ?",
+    'Je te souhaite une belle journée !',
+    "A la recherche d'Odoo...",
+    'Cuisson en cours...',
+    "Vers l'infini et l'au delà !",
+    'Il était une fois...',
+    'Dans une galaxie très très lointaine...',
+];
 
 export default class Initialising extends React.Component<InitialisingProps> {
     constructor(props: InitialisingProps) {
@@ -29,6 +50,7 @@ export default class Initialising extends React.Component<InitialisingProps> {
     }
 
     componentDidMount(): void {
+        return;
         Database.sharedInstance()
             .migrate()
             .then(success => {
@@ -61,6 +83,9 @@ export default class Initialising extends React.Component<InitialisingProps> {
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>Chargement...</Text>
+                <View style={styles.quoteView}>
+                    <Text style={styles.quoteText}>{welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)]}</Text>
+                </View>
             </View>
         );
     }
