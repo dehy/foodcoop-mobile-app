251b0f2d57f1fd4e1b4be1a510c4e74dab34fa10
Arnaud de Mouhy <arnaud.demouhy@supercoop.fr>
2023-01-19 13:56:21 +0100

    fix: do not minify even on release builds

--- metro.config.js
+++ metro.config.js
@@ -20,6 +20,9 @@ module.exports = (async () => {
                 },
             }),
             babelTransformerPath: require.resolve('react-native-svg-transformer'),
+            minifierConfig: {
+                mangle: false,
+            },
         },
         resolver: {
             assetExts: assetExts.filter(ext => ext !== 'svg'),
